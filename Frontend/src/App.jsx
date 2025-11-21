0650d7c115e6a64c3ff8b1c8cbcc64236db40858
Amritesh <amriteshkumarrai14@gmail.com>
2025-11-21 16:33:10 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import LandingPage from './pages/LandingPage'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <LandingPage />
+      <Footer />
+    </>
+  )
+}
+
+export default App
